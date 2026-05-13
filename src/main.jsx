346e52b70ae79be006cbd11d7acc68e87ef48041
Marwadi Solution <marwadisolution@gmail.com>
2026-05-13 20:31:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import App from "./App";
-import PrivacyPolicy from "./PrivacyPolicy";
+import App from "./App.jsx";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MarwadisSolution">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
